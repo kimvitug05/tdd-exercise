c95ca8190a98b727d0930d2ac3b15d520acc69f1
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-09-15 17:41:29 -0700

    Add blackjack_score method solution and tests

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -3,5 +3,33 @@
 VALID_CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
 
 def blackjack_score(hand)
+  score = 0
 
+  hand.each do |card|
+    case card
+    when 'Jack', 'Queen', 'King'
+      score += 10
+    when 'Ace'
+      score += 11
+    else
+      card.class == Integer && card >= 2 && card <= 10 ? score += card : (raise ArgumentError, "Invalid card: #{card}")
+    end
+  end
+
+  if score > 21
+    aces = hand.count('Ace')
+
+    while aces > 0
+      score -= 10
+
+      break if score <= 21
+
+      aces -= 1
+    end
+  end
+
+  raise ArgumentError, "Bust!" if score > 21
+
+  return score
 end
+
--- test/blackjack_score_test.rb
+++ test/blackjack_score_test.rb
@@ -17,27 +17,75 @@ describe 'Blackjack Score' do
     # Act
     score = blackjack_score(hand)
 
-    # Assert <-  You do this part!
+    # Assert
+    expect(score).must_equal 7
 
   end
 
   it 'facecards have values calculated correctly' do
 
+    # Arrange
+    hand = ['King', 'Queen']
+
+    # Act
+    score = blackjack_score(hand)
+
+    # Assert
+    expect(score).must_equal 20
+
   end
 
   it 'calculates aces as 11 where it does not go over 21' do
+
+    # Arrange
+    hand = ['Ace', 9]
+
+    # Act
+    score = blackjack_score(hand)
+
+    # Assert
+    expect(score).must_equal 20
 
   end
 
   it 'calculates aces as 1, if an 11 would cause the score to go over 21' do
 
+    # Arrange
+    hand = ['Queen', 'King', 'Ace']
+
+    # Act
+    score = blackjack_score(hand)
+
+    # Assert
+    expect(score).must_equal 21
+
   end
 
   it 'raises an ArgumentError for invalid cards' do
 
+    # Arrange
+    hand = ['nine', 'hotdog']
+
+    # Assert - Act
+    exception = assert_raises ArgumentError do
+      blackjack_score(hand)
+    end
+
+    assert_equal('Invalid card: nine', exception.message)
+
   end
 
   it 'raises an ArgumentError for scores over 21' do
+
+    # Arrange
+    hand = ['King', 'Queen', 2]
+
+    # Assert - Act
+    exception = assert_raises ArgumentError do
+      blackjack_score(hand)
+    end
+
+    assert_equal('Bust!', exception.message)
 
   end
 end
